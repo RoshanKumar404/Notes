74bd76389ca4ac3f3901c8f907d69b6170956658
RoshanKumar404 <roshankuar951@gmail.com>
2024-05-01 00:58:45 +0530

    just added some thing

--- src/screens/NotesScreen.tsx
+++ src/screens/NotesScreen.tsx
@@ -1,12 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 export default function NotesScreen() {
   return (
-    <View>
-      <Text>NotesScreen</Text>
+    <View style={styles.background}> 
+      <Text style={{fontSize:20}}>NotesScreen</Text>
+      <View>
+        <TouchableOpacity onPress={()=>console.log("jai sri ram")}>
+           <Text>add one</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  background:{
+    backgroundColor:"#00385a",
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
