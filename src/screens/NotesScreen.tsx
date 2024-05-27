8477161145a39932787152685dfe8546b4e94de8
RoshanKumar404 <roshankuar951@gmail.com>
2024-05-27 17:40:32 +0530

    dss

--- src/screens/NotesScreen.tsx
+++ src/screens/NotesScreen.tsx
@@ -1,43 +1,81 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native';
+import { Button, Modal, TextInput } from 'react-native-paper';
 
 export default function NotesScreen() {
   const [isVisible, setIsVisible] = useState(true);
   const [notes, setNotes] = useState('');
   const [addons, setAddons] = useState([]);
+  const [greet, setGreet] = useState('');
+  const [onmodal, setOnModal] = useState(false);
+  const [title, setTitle] = useState('');
 
-  const toggleTextInput = () => {
-    setIsVisible(!isVisible);
-    if (notes.trim() !== '') {
-      setAddons(prevAddons => [...prevAddons, notes]);
+  const findGreet = () => {
+    const hrs = new Date().getHours();
+    if (hrs === 0 || hrs < 12) return 'Morning';
+    if (hrs === 1 || hrs < 17) return 'Afternoon';
+    return 'Evening';
+  };
+
+  useEffect(() => {
+    setGreet(findGreet());
+  }, []);
+
+  const toggleTextEntry = () => {
+    setOnModal(!onmodal);
+  };
+
+  const addNote = () => {
+    if (notes.trim() !== '' && title.trim() !== '') {
+      setAddons(prevAddons => [...prevAddons, { id: Date.now(), title: title, text: notes }]);
       setNotes('');
+      setTitle('');
+      setOnModal(false);
     }
   };
 
+  const renderItem = ({ item }) => (
+    <View style={styles.noteContainer}>
+      <Text style={styles.noteTitle}>{item.title}</Text>
+      <Text style={styles.note}>{item.text}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.background}>
-      {isVisible && (
-        <TextInput
-          placeholder="Plz Add your notes here"
-          style={{ marginTop: 10 }}
-          multiline={true}
-          value={notes}
-          onChangeText={text => setNotes(text)}
-        />
-      )}
-      <View style={styles.notesContainer}>
-        {addons.map((note, index) => (
-          <Text key={index} style={styles.note}>
-            {note}
-          </Text>
-        ))}
-      </View>
-      <View style={styles.addButtonContainer}>
-        <TouchableOpacity onPress={toggleTextInput}>
-          <Text style={styles.addButton}>+</Text>
-        </TouchableOpacity>
-      </View>
+      <Text style={{ fontSize: 28, fontWeight: 'bold', color: '#EA7773' }}>Hello Roshan, Good {greet}</Text>
+      <Modal visible={onmodal} onDismiss={() => setOnModal(false)}>
+        <View style={{ backgroundColor: 'white', padding: 20 }}>
+          <TextInput
+            placeholder="Title"
+            style={{ marginTop: 10, paddingHorizontal: 10 }}
+            value={title}
+            onChangeText={text => setTitle(text)}
+            autoFocus={onmodal}
+          />
+          <TextInput
+            placeholder="Please add your notes here"
+            style={{ marginTop: 10, paddingHorizontal: 10 }}
+            multiline={true}
+            value={notes}
+            onChangeText={text => setNotes(text)}
+          />
+          <Button mode="contained" onPress={addNote} style={{ marginTop: 10 }}>
+            Add Note
+          </Button>
+        </View>
+      </Modal>
+      <FlatList
+        data={addons}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        contentContainerStyle={{ alignItems: 'flex-start' }}
+        style={styles.notesContainer}
+      />
+      <TouchableOpacity style={styles.addButtonContainer} onPress={toggleTextEntry}>
+        <Text style={styles.addButton}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -52,20 +90,29 @@ const styles = StyleSheet.create({
   },
   notesContainer: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    marginTop: 20,
-    marginLeft: 20,
+    width: '100%',
+    paddingVertical: 20,
   },
-  note: {
+  noteContainer: {
+    flex: 1,
+    width: '50%',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  noteTitle: {
     fontSize: 18,
     color: 'white',
+    fontWeight: 'bold',
     marginBottom: 5,
+  },
+  note: {
+    fontSize: 16,
+    color: 'white',
   },
   addButtonContainer: {
     position: 'absolute',
-    bottom: 30,
-    right: 20,
+    bottom: 50,
+    right: 40,
   },
   addButton: {
     fontSize: 70,
